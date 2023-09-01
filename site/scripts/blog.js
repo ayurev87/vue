ded7dfc8c6aaed5e2bd647954c1bdeef06e04fc2
ayurev87 <a-yurev_87@mail.ru>
2023-09-01 23:58:35 +0300

    update site

---
+++ site/scripts/blog.js
@@ -0,0 +1,59 @@
+new Vue({
+  el: "#app",
+  data: {
+    news: [
+      {
+        id: 1,
+        img: "./img/news_1.png",
+        category: "Kitchan Design",
+        title: "Let’s Get Solution For Building Construction Work",
+        date: "26 December,2022 ",
+      },
+      {
+        id: 2,
+        img: "./img/news_2.png",
+        category: "Living Design",
+        title: "Low Cost Latest Invented Interior Designing Ideas",
+        date: "22 December,2022 ",
+      },
+      {
+        id: 3,
+        img: "./img/news_3.png",
+        category: "Interior Design",
+        title: "Best For Any Office & Business Interior Solution",
+        date: "25 December,2022 ",
+      },
+      {
+        id: 4,
+        img: "./img/news_4.png",
+        category: "Kitchan Design",
+        title: "Let’s Get Solution For Building Construction Work",
+        date: "26 December,2022 ",
+      },
+      {
+        id: 5,
+        img: "./img/news_5.png",
+        category: "Living Design",
+        title: "Low Cost Latest Invented Interior Designing Ideas",
+        date: "22 December,2022 ",
+      },
+      {
+        id: 6,
+        img: "./img/news_6.png",
+        category: "Interior Design",
+        title: "Best For Any Office & Business Interior Solution",
+        date: "25 December,2022 ",
+      },
+    ],
+    post: {
+      id: 1,
+      img: "./img/PostImg.png",
+      title: "Low Cost Latest Invented Interior Designing Ideas",
+      shortDescription:
+        "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpis dignissim maximus.posuere in.Contrary to popular belief.",
+      mainThing:
+        "Lorem Ipsum is not simply random text. It has roots in a piece of classica.",
+      date: "26 December,2022 ",
+    },
+  },
+});
