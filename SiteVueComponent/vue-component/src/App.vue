c2756351988b5b2e8d245da61fec5a761b8e0d41
ayurev87 <a-yurev_87@mail.ru>
2023-09-08 00:28:17 +0300

    addSiteComponent

---
+++ SiteVueComponent/vue-component/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HeaderBlock/>
+</template>
+
+<script>
+import HeaderBlock from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBlock,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_vars.scss';
+@import '@/assets/scss/fonts.scss';
+
+</style>
---
+++ SiteVueComponent/vue-component/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+   <header class="header">
+            <div class="container menu">
+                <div class="menu__left">
+                    <a href="index.html" class="menu__logo_link">
+                        <svg width="34" height="34" viewBox="0 0 34 34">
+                            <path class="logo"></path>
+                        </svg>
+                        <h4 class="menu__logo_name">Interno</h4>
+                    </a>
+                </div>
+                <div class="menu__right">
+                    <nav class="menu_links">
+                        <ul class="links-main">
+                            <li class="links-main__link" v-for="menu in menus" :key="menu.id">
+                                <a href=menu.url>
+                                    <h7 class="nav__text">{{menu.name}}</h7>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderBlock',
+
+    data() {
+        return {
+            menus: [{ url: '@/index.html', name: 'Home' }, { url: '', name: 'Project' }, { url: 'articles.html', name: 'Articles.html' }, { url: 'blog.html', name: 'Blog' }]
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+
+
+</style>
