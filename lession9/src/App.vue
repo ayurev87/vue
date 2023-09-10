675a954df77065acdeda4d58244659dd000ea8f6
ayurev87 <a-yurev_87@mail.ru>
2023-09-11 01:46:03 +0300

    Add lession 9

---
+++ lession9/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <CalcFunc/>
+  </div>
+
+</template>
+
+<script>
+import CalcFunc from './components/CalcFunc.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalcFunc
+}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
