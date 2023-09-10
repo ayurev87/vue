395bc52d9826f1c06ebc95f6dcd2e6a0ad6c465c
ayurev87 <a-yurev_87@mail.ru>
2023-09-11 01:46:39 +0300

    Add product site

---
+++ product-site/src/components/ProductDetails.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ProductItem 
+        v-for="product in products" 
+        :key="product.id" 
+        :product="product"/>
+    </div>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue'
+
+
+export default {
+    name: 'ProductDetails',
+    components: {
+        ProductItem,
+    },
+
+    data() {
+        return {
+            products: [
+                { id: 1, name: 'product 1', price: 250, available: true },
+                { id: 2, name: 'product 2', price: 150, available: false },
+                { id: 3, name: 'product 3', price: 220, available: true },
+                { id: 4, name: 'product 4', price: 90, available: false },
+                {id:5, name: 'product 5', price: 150, available: true  },
+            ]
+
+            
+        };
+    },
+
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ product-site/src/components/ProductItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="product">
+        <h2>Наименование товара: {{product.name}}</h2>
+        <h3>Цена в рублях: {{product.price}}₽</h3>
+        <h3>Цена в долларах: {{RubToDol(product.price)}}$ </h3>
+        <h3 v-if="product.available" class="green">Availble</h3>
+        <h3 v-else class="red">Out of stock {{product.available}}</h3>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductItem',
+    props: ['product'],
+    data() {
+        return {
+           
+        };
+    },
+
+
+    methods: {
+        RubToDol(price) {
+            return  price / 100;
+        }
+    },
+};
+</script>
+
+<style lang="css" scoped>
+.product{
+    border: 1px solid black;
+    margin: 10px;
+    border-radius: 10px;
+    width: 500px;
+   
+}
+.green {
+    color: green;
+}
+.red {
+    color: red;
+}
+</style>
