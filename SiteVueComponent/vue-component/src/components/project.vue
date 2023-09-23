6fc16a328bbfc531f675d7b6b40aef2e898cd3bd
ayurev87 <a-yurev_87@mail.ru>
2023-09-23 23:54:43 +0300

    fin

--- SiteVueComponent/vue-component/src/components/project.vue
+++ SiteVueComponent/vue-component/src/components/project.vue
@@ -13,14 +13,14 @@
         <div class="container">
             <div class="project__nav">
                 <ul class="project__nav_ul">
-                    <li class="project__nav_li" @click="selectNav(item_nav)" v-for="item_nav in nav" :key="item_nav.id">
+                    <li class="project__nav_li" @click="selectNav(item_nav) , activeNav = index" v-for="(item_nav, index) in nav" :key="index" :class="{active: activeNav === index}">
                         <h8>{{ item_nav }}</h8>
                     </li>
                 </ul>
 
             </div>
             <div class="project__categories">
-                <div class="project__contnet" v-for="image in projectFilter()" :key="image.id">
+                <div class="project__contnet" v-for="image in numItem()" :key="image.id">
                     <img class="project__image" :src="image.img" />
                     <div class="project__subimage">
                         <div class="project__text">
@@ -37,12 +37,31 @@
 
                 </div>
             </div>
-<ul class="project__numbers">
-    <li class="project__number" v-for="number in numbersCount()" :key="number.id">
-        <p>0{{number}}</p>
+            <ul class="project__numbers">
+                <li class="project__number" v-if="previous">
+                    <svg width="52" height="53" viewBox="0 0 52 53">
+                        <g id="svg_3" transform="rotate(180 26 26.267)">
+                            <circle cx="26" cy="26.267" r="26" fill="#Fff" />
+                            <path class="minright"></path>
+                        </g>
+                    </svg>
 
-    </li>
-</ul>
+                </li>
+                <div @click="selectNum(index), activeNum = index" v-for="(number, index) in numbersCount()" :key="index" >
+                    <li class="project__number" v-if="minMaxNum(index)" :class="{active: activeNum===index}">
+                        <p>0{{ number }}</p>
+
+                    </li>
+                </div>
+                <li class="project__number" v-if="next">
+                    <svg width="52" height="53" viewBox="0 0 52 53">
+
+                        <circle cx="26" cy="26.267" r="26" fill="#Fff" />
+                        <path class="minright"></path>
+                    </svg>
+
+                </li>
+            </ul>
 
         </div>
     </section>
@@ -58,7 +77,28 @@ export default {
     data() {
         return {
             nav: ['Bathroom', 'Bed Room', 'Kitchen', 'Living Area'],
+            nav_active: '',
             project_image: [
+                { premises: 'Bed Room', name: 'Minimal Bedroom', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/project_bed_room1.png`) },
+                { premises: 'Bed Room', name: 'Minimal Bedroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room2.png`) },
+                { premises: 'Bed Room', name: 'Classic Minimal Bedroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room3.png`) },
+                { premises: 'Bed Room', name: 'Modern Bedroom', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/project_bed_room4.png`) },
+                { premises: 'Bed Room', name: 'Minimal Bedroom table', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room5.png`) },
+                { premises: 'Bed Room', name: 'System Table', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room6.png`) },
+                { premises: 'Bed Room', name: 'Modern Medroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room7.png`) },
+                { premises: 'Bed Room', name: 'Modern Bedroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room8.png`) },
+                { premises: 'Kitchen', name: 'Modern Kitchen', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/kitchen1.png`) },
+                { premises: 'Kitchen', name: 'Modern Kitchen', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/kitchen2.png`) },
+                { premises: 'Kitchen', name: 'Modern Kitchen', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/news_1.png`) },
+                { premises: 'Kitchen', name: 'Modern Kitchen', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/news_2.png`) },
+                { premises: 'Kitchen', name: 'Modern Kitchen', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/news_3.png`) },
+                { premises: 'Kitchen', name: 'Modern Kitchen', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/news_4.png`) },
+                { premises: 'Living Area', name: 'Living Area', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/news_5.png`) },
+                { premises: 'Living Area', name: 'Living Area', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/gallary4.png`) },
+                { premises: 'Bathroom', name: 'Modern Bathroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_Bathroom1.png`) },
+                { premises: 'Bathroom', name: 'Modern Bathroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project__bathroom2.jpg`) },
+                { premises: 'Bathroom', name: 'Modern Bathroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project__bathroom3.jpg`) },
+                { premises: 'Bathroom', name: 'Modern Bathroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project__bathroom4.jpg`) },
                 { premises: 'Bed Room', name: 'Minimal Bedroom', subtitle: 'Decor / Artchitecture', star: true, img: require(`@/assets/project_bed_room1.png`) },
                 { premises: 'Bed Room', name: 'Minimal Bedroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room2.png`) },
                 { premises: 'Bed Room', name: 'Classic Minimal Bedroom', subtitle: 'Decor / Artchitecture', star: false, img: require(`@/assets/project_bed_room3.png`) },
@@ -83,18 +123,37 @@ export default {
             current_nav: '',
             numbers: [],
             current_num: 1,
+            previous: false,
             minItem: 0,
             maxItem: 7,
+            next: false,
+            activeNav: undefined,
+            activeNum: 0,
 
         };
     },
 
-    mounted() {
-
-
-    },
 
     methods: {
+
+        minMaxNum(id) {
+            if (this.current_num < 3) { this.previous = false; }
+            else {
+                if (this.current_num - 2 > id) {
+                    return false;
+                }
+                this.previous = true;
+            }
+            if (this.id < this.current_num + 1) this.next = false;
+            if (id > this.current_num + 1) {
+                this.next = true
+                return false;
+
+            }
+            this.next = false;
+            return true;
+
+        },
         projectFilter() {
             if (this.current_nav === '') return this.project_image
             else return this.project_image.filter((image) => {
@@ -104,21 +163,39 @@ export default {
         selectNav(button) {
             return this.current_nav = button;
         },
+        selectNum(button) {
+            let numerInterval = 8 * button;
+            this.minItem = 0 + numerInterval;
+            this.maxItem = 7 + numerInterval;
+            return this.current_num = button + 1;
+        },
         numbersCount() {
-            let items = this.projectFilter().length/8;
+            let items = this.projectFilter().length / 8;
             let array = [];
-            if (array.length < items) {
-                for (let index = array.length+1; index <= items; index++) {
+            if (items < 1) array.push(1);
+            
+            else if (array.length < items) {
+                for (let index = array.length + 1; index <= items; index++) {
                     array.push(index);
                 }
             } else if (array.length > items) {
                 for (let index = array.length; index > items; index--)
                     array.pop();
             }
+
+            
             return this.numbers = array;
         },
         numItem() {
-            
+            let arrayProjectFilter = this.projectFilter();
+            let dataFromProject = [];
+            for (let index = this.minItem; index <= this.maxItem; index++) {
+                dataFromProject.push(arrayProjectFilter[index]);
+                if (arrayProjectFilter.length-1 <= index) break;
+                
+            }
+            return dataFromProject;
+
         }
 
 
@@ -126,6 +203,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+.active{
+    background-color: #cda274;
+    color: #fff;
+}
+
+
 
 </style>
