28518eb34a81277a071517bf3d35f1d915c257e8
ayurev87 <a-yurev_87@mail.ru>
2024-01-25 09:15:34 +0300

    add vuex ROUTER

--- vuex/src/main.js
+++ vuex/src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueRouter from 'vue-router'
+import routes from '@/router'
 
-createApp(App).use(store).mount('#app')
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+const router = new VueRouter({ routes })
+
+new Vue({
+    store,
+    render: h => h(App),
+    router,
+}).$mount('#app')
